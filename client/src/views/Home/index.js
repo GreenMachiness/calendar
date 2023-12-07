96dba4dbf3da7410187de8b2d19e25448c575249
Evan <you@example.com>
2023-12-07 18:50:00 -0500

    added views to calendar

--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import React from "react";
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
-export default class DemoApp extends React.Component {
-  render() {
-    return (
-      <FullCalendar
-        plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth"
+function Calendar() {
+  return (
+    <div>
+      <br></br>
+      <Fullcalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={"dayGridMonth"}
+        headerToolbar={{ // these can be interchanged
+          start: "today prev,next", //selection tools
+          center: "title", // month in the center
+          end: "dayGridMonth,timeGridWeek,timeGridDay", // will give view buttons for month/week/day
+        }}
+        height={"90vh"}
       />
-    )
-  }
+    </div>
+  );
 }
+
+export default Calendar;
