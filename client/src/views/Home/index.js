79ad5322778e51dc71ce6ea7378d0a3597e075bf
Evan <you@example.com>
2023-12-14 16:41:28 -0500

    added notifications

--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -17,11 +17,13 @@ import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
 import { createTask } from "../../utility/api";
-import { fetchMe, fetchTasks, updateTask, deleteTask, createNotification, updateNotification, deleteNotification  } from "../../utility/api";
+import { fetchMe, fetchTasks, updateTask, deleteTask, createNotification, updateNotification, deleteNotification, fetchNotifications  } from "../../utility/api";
 import { isUserLoggedIn, clearToken } from "../../utility/utils";
 import listPlugin from "@fullcalendar/list";
 import Weather from "./weather";
-import rrulePlugin from "@fullcalendar/rrule";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 function Calendar() {
   // want to get the current date
@@ -45,6 +47,11 @@ function Calendar() {
     // console.log("start time:", startTime)
     return startTime.toTimeString().slice(0, 5);
   }
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+  
 
   const priorityColors = {
     optional: "blue",
@@ -89,6 +96,10 @@ function Calendar() {
   const [userId, setUserId] = useState(null);
   const [user, setUser] = useState(null);
   const [selectedTask, setSelectedTask] = useState(null);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [open, setOpen] = useState(false);
+
+
 
   useEffect(() => {
     // check if user is logged in
@@ -171,6 +182,84 @@ function Calendar() {
     }
   };
 
+//   const timestamp = 1702588880904;
+// const date = new Date(timestamp);
+// console.log(date);
+
+  useEffect(() => {
+
+    //need a function to fetch the notifications from my notifications database, get also tasks, to refer the taskId from the notifications table
+    const checkNotifications = async () => {
+      try {
+        //need variables for each response, fetch from databsase, make a promise to see if resolved.
+        const [notificationsResponse, tasksResponse] = await Promise.all([
+          fetchNotifications(),
+          fetchTasks()
+        ]);
+        // set variables, getting error  because  array is empty, make an or statement so that it wouldnt error out if the databases are empty
+        const notifications = notificationsResponse.data || [];
+        const tasks = tasksResponse.data || [];
+        // console.log('tasks here', tasks)
+        //make current time variable to get timestamp
+        const currentTime = new Date().getTime();
+        //for loop didnt work for me, try other methods, tried .map didnt work, .forEach worked.
+        notifications.forEach((notification) => {
+          // console.log("ntoification here:", notification)
+          //get data from each notification
+          const { taskId, duration, unit } = notification;
+          // make tasknotify the task associated with the taskID from my notifications table
+          const taskNotify = tasks.find((task) => task.id === taskId);
+          // console.log("tasknotify here:", taskNotify.start)
+          
+          if (taskNotify) {
+            let notificationTime = new Date(taskNotify.start).getTime();
+            console.log("tasknotify here:", taskNotify)
+            // calculate in milliseconds for each unit.
+            if (unit === 'minutes') {
+              notificationTime -= duration * 60 * 1000;
+            } else if (unit === 'hours') {
+              notificationTime -= duration * 60 * 60 * 1000;
+            } else if (unit === 'days') {
+              notificationTime -= duration * 24 * 60 * 60 * 1000;
+            } else if (unit === 'weeks') {
+              notificationTime -= duration * 7 * 24 * 60 * 60 * 1000;
+            }
+            // offset for hours is 5, issue with timezone, cant figure out the timezone issue, plus 5 the hours to fix for now
+            notificationTime += 5 * 60 * 60 * 1000;
+            // check notification time if its less than the current time, trigger a notification snackbar if it is notification time,
+            if (notificationTime <= currentTime) {
+              console.log("notify at this time", currentTime)
+              //sset sncakbar message
+              setSnackbarMessage(`Task "${taskNotify.title}" is starting soon!`);
+              setOpen(true);
+              setTimeout(async () => {
+                setOpen(false);
+                //need to delete notification because it keeps rendering on the app even after notificationTime. 
+                try {
+                  await deleteNotification(notification.id);
+                  console.log(`Notification ${notification.id} deleted`);
+                } catch (error) {
+                  console.error(`Error deleting notification ${notification.id}:`, error.message);
+                }
+                //set 6 seconds for the snackbar to close automatically
+              }, 6000);
+            }
+          }
+        });
+      } catch (error) {
+        console.error('Error fetching notifications:', error);
+      }
+    };
+  
+    // set interval for checking database. 
+    const intervalId = setInterval(() => {
+      checkNotifications();
+    }, 60000); 
+  
+    // Clear interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+  
   const handleNotifyValueChange = (e) => {
     setNotifyValue(e.target.value);
   };
@@ -190,6 +279,14 @@ function Calendar() {
   const handleRepetitionUnitChange = (e) => {
     setRepetitionUnit(e.target.value);
   };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
   //full calendar has handle event click on their docs to get data of the task when clicked.
   const handleEventClick = async (clickInfo) => {
     //something wrong with the start and end, wrong format.
@@ -287,7 +384,7 @@ function Calendar() {
           unit: notifyUnit,
           taskId: createdTask.data.id,
         };
-        console.log("check here:", createdTask)
+        // console.log("check here:", createdTask)
   
         await createNotification(notificationData);
       }
@@ -545,6 +642,14 @@ function Calendar() {
           />
         </div>
       </div>
+
+      <Stack spacing={2} sx={{ width: '100%' }}>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="info" sx={{ width: '100%' }}>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+    </Stack>
     </div>
   );
 }
